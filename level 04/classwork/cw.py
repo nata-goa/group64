6b31fef14c8d675c530406d916fc2f45bf2f6b0f
nata-goa <bajelidzenata0@gmail.com>
2025-02-22 22:19:50 +0400

    fifth hw

---
+++ level 04/classwork/cw.py
@@ -0,0 +1,29 @@
+a = 10 + 5
+print(a)
+
+b = 20 - 7
+print(b)
+
+c = 4 * 6
+print(c)
+
+d = 15 / 4
+print(d)
+
+
+level = 0
+level = 5
+
+for i in range (3)
+level += 1
+
+
+fullname = "natali bajelidze"
+hobby = "cooking"
+
+print(fullname)
+print(hobby) 
+
+print(12)
+print(8+2)
+
---
+++ level 04/homeworkk/hw.py
@@ -0,0 +1,16 @@
+mom = "mari"
+dad = "dato"
+bro = "niko"
+print("my mom's name is " + mom +". my father's name is"+dad+" my siblings name is "+bro)
+
+a = 5 + 3
+print(a)
+
+b = 15 - 7
+print(b)
+
+c = 6 * 4
+print(c)
+
+d = 17 // 4
+print(d)
