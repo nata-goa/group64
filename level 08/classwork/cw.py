3cde9e8956da637dac87e9c1a3e055ebb8423394
nata-goa <bajelidzenata0@gmail.com>
2025-03-09 19:55:33 +0400

    eighth hw

---
+++ level 08/classwork/cw.py
@@ -0,0 +1,11 @@
+name = input ("your name: ")
+surname = input ("your surname:")
+print (name + "" + surname) 
+
+number1 = input ("enter number 1:")
+number2 = input ("enter number 2:")
+
+print (number1 + number2)
+
+level = input ("enter your level:")
+print  (level = 1) 
