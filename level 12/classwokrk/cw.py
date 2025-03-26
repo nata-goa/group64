720d11ec1bc849677c72e63c819ff2f1a86e468e
nata-goa <bajelidzenata0@gmail.com>
2025-03-27 00:15:45 +0400

    eleventh hw

---
+++ level 12/classwokrk/cw.py
@@ -0,0 +1,29 @@
+print(5 > 3) #true
+print(5 < 3) #false
+print(5 >= 3) #true
+print(5 <= 3) #false
+print(5 == 3) #false
+print(5 != 3) #true4
+
+
+
+#NOT 
+print(not True)   #False
+print(not False)  #True
+
+#OR 
+print(True or True)   #True
+print(True or False)  #True
+print(False or True)  #True
+print(False or False) # False
+
+# ND 
+print(True and True)   #True
+print(True and False)  #False
+print(False and True)  #False
+print(False and False) #False
+
+
+
+
+
---
+++ level 14/classwork/cw.py
@@ -0,0 +1,6 @@
+for num in range(1,11):
+    print(num)
+
+    name= "natali"
+for letter in name:
+    print(letter)
