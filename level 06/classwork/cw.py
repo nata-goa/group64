7d14ae3cef3e64616a209919065f53e7edf0ed75
nata-goa <bajelidzenata0@gmail.com>
2025-04-01 17:59:58 +0400

    seventh hw

--- level 06/classwork/cw.py
+++ level 06/classwork/cw.py
@@ -1,1 +1,6 @@
-# 
+#snake case არის სახელწოდების სტილი სადაც სიტყვები _ გამოიყოფა
+#case sensitivity ნიშნავს რომ დიდი და პატარა ასოები განსხვავებულად აღიქმება
+
+int(input("enter num"))
+num1 = int(input("enter num"))
+
